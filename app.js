13bbb84ca434fa489cc0459295a46cb5734e2496
michaelsetlock <Michaelsetlock17@augustana.edu>
2022-02-15 14:15:11 -0600

    Updated styling with Bulma classes

--- app.js
+++ app.js
@@ -9,49 +9,51 @@ const winningScoreSelect = document.querySelector('#goal');
 let winningScore = 2;
 let gameOver = false;
 
-winningScoreSelect.addEventListener('change', function(){
-    winningScore=parseInt(this.value)
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value)
     res()
 })
 
 p1Btn.addEventListener('click', function () {
-    if (!gameOver){
+    if (!gameOver) {
         p1P += 1;
 
         if (p1P === winningScore) {
-        gameOver=true;
-        p1Score.classList.add("winner")
-        p2Score.classList.add("loser")
+            gameOver = true;
+            p1Score.classList.add("has-text-success")
+            p2Score.classList.add("has-text-danger")
 
-        
+
+        }
+        p1Score.textContent = p1P;
+
     }
-    p1Score.textContent = p1P;
-
-}
 })
 p2Btn.addEventListener('click', function () {
-    if (!gameOver){
+    if (!gameOver) {
         p2P += 1;
 
         if (p2P === winningScore) {
-        gameOver=true;
-        p2Score.classList.add("winner")
-        p1Score.classList.add("loser")
+            gameOver = true;
+            p2Score.classList.add("has-text-success")
+            p1Score.classList.add("has-text-danger")
+
+        }
+        p2Score.textContent = p2P;
 
     }
-    p2Score.textContent = p2P;
-
-}
 })
 reset.addEventListener('click', res)
 
-function res(){
+function res() {
     gameOver = false;
-    p1P= 0;
-    p2P=0;
+    p1P = 0;
+    p2P = 0;
     p1Score.textContent = p1P;
     p2Score.textContent = p2P;
-    p1Score.classList.remove("winner", "loser")
-    p2Score.classList.remove("loser", "loser")
+    p1Score.classList.remove("has-text-success",
+    "has-text-danger")
+    p2Score.classList.remove("has-text-success"
+    ,"has-text-danger")
 
 }
